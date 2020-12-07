8b429c817942ccb1dd49868b08e4695c644e703e
Maci <marcel.schmidt@edu.campus02.at>
2020-12-07 11:47:50 +0100

    newPassword Validator implemented

--- Forum/src/app/shared/validation/new-password.directive.ts
+++ Forum/src/app/shared/validation/new-password.directive.ts
@@ -1,29 +1,37 @@
-import { Directive } from '@angular/core';
-import {AbstractControl, NG_VALIDATORS, ValidationErrors} from '@angular/forms';
+import {Directive, Input} from '@angular/core';
+import {AbstractControl, NG_ASYNC_VALIDATORS, ValidationErrors} from '@angular/forms';
+import { UserService } from '../services/user.service';
+import {HttpParams} from '@angular/common/http';
+import {User} from '../entities/user';
+import {map} from 'rxjs/operators';
+import {Observable} from 'rxjs';
 
 @Directive({
-  selector: '[appNewPassword]',
+  selector: 'input[appNewPassword]',
   providers: [{
-    provide: NG_VALIDATORS,
+    provide: NG_ASYNC_VALIDATORS,
     useExisting: NewPasswordDirective,
     multi: true
   }]
 })
 export class NewPasswordDirective {
 
-  constructor() { }
+  @Input() curUser: User;
 
-  validate(c: AbstractControl): ValidationErrors | null {
-    const validCities: string[] = ['Hamburg', 'Graz'];
-    console.log(c.value);
-    if (c.value && validCities.indexOf(c.value) === -1) {
-      return {
-        city: {
-          actualValue: c.value,
-          validCities
-        }
-      };
-    }
-    return null;
+  constructor(private userService: UserService) { }
+
+  validate(c: AbstractControl): Promise<ValidationErrors> | Observable<ValidationErrors> | null {
+    const params = new HttpParams()
+      .set('id', String(this.curUser.id))
+      .set('password', c.value);
+
+    return this.userService.getUser(params).pipe(map((response) =>
+    {
+      if (response.length !== 1) {
+        return null;
+      }
+
+      return {appNewPassword: true};
+    }));
   }
 }
